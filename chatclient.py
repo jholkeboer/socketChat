94c2bf34f7b10e2c2dd2d7536d8dc4bb8ab114e7
jholkeboer <jholkeboer@users.noreply.github.com>
2016-02-05 22:38:02 -0800

    client input loop

--- chatclient.py
+++ chatclient.py
@@ -15,11 +15,7 @@ if len(sys.argv) != 3:
 host = sys.argv[1]
 port = int(sys.argv[2])
 print 'Welcome to the socketChat client.'
-
-
-
-# get server hostname, port number, and handle from user
-
+handle = raw_input('Choose your handle: ')
 
 # create socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,6 +24,10 @@ try:
     # print 'Attempting connection to host %s on port %d ...' % [host,port]
     host_ip = socket.gethostbyname(host)
     sock.connect((host_ip, port))
+    msg = raw_input("%s> " % handle)
+    while (msg != '\quit'):
+        sock.send(msg)
+        msg = raw_input("%s> " % handle)
 except socket.gaierror:
     print 'Could not find host. Exiting.'
     sys.exit()
