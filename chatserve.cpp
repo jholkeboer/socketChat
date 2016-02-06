7382fb355a67a9618a04ab8501dc9cae2a7b965f
jholkeboer <jholkeboer@users.noreply.github.com>
2016-02-06 14:31:29 -0800

    change buffer clear

--- chatserve.cpp
+++ chatserve.cpp
@@ -141,7 +141,7 @@ int main(int argc, char *argv[]) {
                     // get user message input
                     std::cin.getline(message, 500);
                     std::cin.clear();
-                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    std::cin.ignore(500, '\n');
                     
                     // copy handle and message to buffer
                     i = 0;
